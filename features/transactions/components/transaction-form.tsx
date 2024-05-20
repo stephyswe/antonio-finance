d6b116af402fcaac3c629f8dcb8b03b29e8703e2
Stephanie <stephaniiehallberg@gmail.com>
2024-05-20 12:35:00 +0200

    fix: remove log in transaction-form / features

--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -67,7 +67,6 @@ export const TransactionForm = ({
   });
 
   const handleSubmit = (values: FormValues) => {
-    console.log({ values });
     const amount = parseFloat(values.amount);
     const amountInMiliunits = convertAmountToMiliunits(amount);
     onSubmit({
