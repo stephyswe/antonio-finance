9c36fb7fae9dc48cb6a24412792cf304a0ee8713
Stephanie <stephaniiehallberg@gmail.com>
2024-05-26 15:54:13 +0200

    19: fix formatPrecentage, addPrefix true

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -88,11 +88,11 @@ export const DataCard = ({
           className={cn(
             "text-muted-foreground text-sm line-clamp-1",
             percentageChange > 0 && "text-emerald-500",
-            percentageChange < 0 && "text-rose-500",
-            
+            percentageChange < 0 && "text-rose-500"
           )}
         >
-          {formatPercentage(percentageChange)} from last period
+          {formatPercentage(percentageChange, { addPrefix: true })} from last
+          period
         </p>
       </CardContent>
     </Card>
